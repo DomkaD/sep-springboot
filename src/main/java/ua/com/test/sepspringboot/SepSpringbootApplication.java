9ecf170727a12b0b355e7c12a1c3983a73ff2ba3
DkDeniss <kvaskasdomkas@gmail.com>
2023-03-28 14:00:23 +0300

    Init

---
+++ src/main/java/ua/com/test/sepspringboot/SepSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ua.com.test.sepspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/test/sepspringboot/SepSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.test.sepspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
